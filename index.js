7babc6811d37b945c48a36c65747fa8bdb3a21e4
한정/루키사랑TF/NE <hanjung@nhnent.com>
2018-02-02 09:32:49 +0900

    remake bundle file

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ import 'highlightjs/styles/github.css';
 import 'tui-editor/dist/tui-editor.css';
 import 'tui-editor/dist/tui-editor-contents.css';
 
-
+console.log("1");
 let hereUri = $(location).attr('href');
+console.log("2");
 
 if(hereUri.search("/mail/read") >= 0){
+    console.log("WHY");
+    
     let content = $("#tuiContentVal").val().split("\n");
     for(let i = 0; i<content.length; i++){
         content[i] = content[i].trim();
         if(content[i].search("data:image")){
-            content[i] = content[i].replace("&lt","<");
-            content[i] = content[i].replace("&gt", ">");
-            content[i] = content[i].replace("&amp","&");
-            content[i] = content[i].replace("&quot","/");
+            content[i] = content[i].replace(/&lt/g,"<");
+            content[i] = content[i].replace(/&gt/g, ">");
+            content[i] = content[i].replace(/&amp/g,"&");
+            content[i] = content[i].replace(/&quot/g,"/");
         }
     }
     content = content.join("\n");
@@ -28,20 +31,21 @@ if(hereUri.search("/mail/read") >= 0){
         viewer: true
     });
 } else if(hereUri.search("/mail/write") >= 0){
-    let replyContent = $("#reply_content").val().split("\n");;
-    for(let i = 0; i<replyContent.length; i++){
-        replyContent[i] = replyContent[i].trim();
-        if(replyContent[i].search("data:image")){
-            replyContent[i] = replyContent[i].replace("&lt","<");
-            replyContent[i] = replyContent[i].replace("&gt", ">");
-            replyContent[i] = replyContent[i].replace("&amp","&");
-            replyContent[i] = replyContent[i].replace("&quot","/");
-        }
-    }
-    replyContent = replyContent.join("\n");
-
-
+    let replyContent = $("#replyContent").val();
+    console.log(replyContent);
     if(replyContent !== ""){
+        replyContent = replyContent.split("\n");;
+        for(let i = 0; i<replyContent.length; i++){
+            replyContent[i] = replyContent[i].trim();
+            if(replyContent[i].search("data:image")){
+                replyContent[i] = replyContent[i].replace(/&lt/g,"<");
+                replyContent[i] = replyContent[i].replace(/&gt/g, ">");
+                replyContent[i] = replyContent[i].replace(/&amp/g,"&");
+                replyContent[i] = replyContent[i].replace(/&quot/g,"/");
+            }
+        }
+        replyContent = replyContent.join("\n");
+
         let sender = $("#originSenderAcnt").val();
         let mailTitle = $("#mailTitle").val();
         let replyString = `\n\n\n-----Original Message-----\n **sender**: ${sender}\n **title**: ${mailTitle}\n\n\n`;
