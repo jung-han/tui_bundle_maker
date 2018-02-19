8a31eba9dd4c76a593741d8584c2d6ece3719946
junghanNHN <hanjung@nhnent.com>
2018-02-19 17:18:16 +0900

    merge 전 푸시.....

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ let hereUri = $(location).attr('href');
 if(hereUri.search("/mail/read") >= 0){
 
     let content = $("#tuiContentVal").val().split("\n");
-    for(let i = 0; i<content.length; i++){
-        content[i] = content[i].trim();
-        if(content[i].search("data:image")){
-            content[i] = content[i].replace(/&lt/g,"<");
-            content[i] = content[i].replace(/&gt/g, ">");
-            content[i] = content[i].replace(/&amp/g,"&");
-            content[i] = content[i].replace(/&quot/g,"/");
-        }
-    }
     content = content.join("\n");
 
     $('#viewerSection').tuiEditor({
@@ -30,18 +21,8 @@ if(hereUri.search("/mail/read") >= 0){
 } else if(hereUri.search("/mail/write") >= 0){
     let replyContent = $("#replyContent").val();
     if(replyContent !== ""){
-        replyContent = replyContent.split("\n");;
-        for(let i = 0; i<replyContent.length; i++){
-            replyContent[i] = replyContent[i].trim();
-            if(replyContent[i].search("data:image")){
-                replyContent[i] = replyContent[i].replace(/&lt/g,"<");
-                replyContent[i] = replyContent[i].replace(/&gt/g, ">");
-                replyContent[i] = replyContent[i].replace(/&amp/g,"&");
-                replyContent[i] = replyContent[i].replace(/&quot/g,"/");
-            }
-        }
+        replyContent = replyContent.split("\n");
         replyContent = replyContent.join("\n");
-
         let sender = $("#originSenderAcnt").val();
         let mailTitle = $("#mailTitle").val();
         let replyString = `\n\n\n-----Original Message-----\n **sender**: ${sender}\n **title**: ${mailTitle}\n\n\n`;
@@ -57,7 +38,17 @@ if(hereUri.search("/mail/read") >= 0){
 
 
     $('#mailForm').on('submit', function(){
-      let mailBody = $("#editSection").tuiEditor("getValue");
-      $('#mailForm').append('<input type="hidden" name="mailBody" value=\''+mailBody+'\'/>');
+        let mailBody = $("#editSection").tuiEditor("getValue");
+        let findTag = ["<script>", "</script>"];
+        for(let i=0; i<findTag.length; i++){
+            let regex = new RegExp(findTag[i], "g");
+            mailBody = mailBody.replace(regex, "");
+        }
+        let regexSingleQuote = new RegExp("'", "g");
+        mailBody = mailBody.replace(regexSingleQuote, "&apos;");
+        let regexDoubleQuote = new RegExp('"', "g");
+        mailBody = mailBody.replace(regexDoubleQuote, "&quot;");
+
+        $('#mailForm').append('<input type="hidden" name="mailBody" value=\''+mailBody+'\'/>');
     });
 }
